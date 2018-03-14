7d37206c1b67fec9266d78b7d3ba479efa7b59e5
Martin Keefe <budha57@hotmail.com>
2018-03-14 19:12:58 +0000

    Renamed source repository.

--- src/js/app/home.js
+++ src/js/app/home.js
@@ -8,8 +8,11 @@ class HomePage extends NormalPage {
 		return (
 			<Fragment>
 				<hr/>
-				<p style={{fontSize: 14+'px', textAlign: 'justify'}}>Techie Note: This site is a simple static single page app hosted on Amazon S3.
-				  If you’re curious you can browse the source code on <a href="https://github.com/martinkeefe/martinkeefe.com-webpack">GitHub</a>.</p>
+				<p style={{fontSize: 14+'px', textAlign: 'justify'}}><b>Techie Note:</b> This site is a simple
+				static single page app powered by <a href="https://reactjs.org/">React</a>, built
+				with <a href="https://webpack.js.org/">WebPack</a> and <a href="https://babeljs.io/">Babel</a>, and hosted
+				on <a href="https://aws.amazon.com/s3/">Amazon S3</a>.  If you’re curious you can browse
+				the source code on <a href="https://github.com/martinkeefe/martinkeefe.com">GitHub</a>.</p>
 			</Fragment>
 		)
 	}
@@ -22,6 +25,6 @@ class HomePage extends NormalPage {
 
 export default function(app) {
 	app.add_route('', context => {
-		app.render(<HomePage {...{app, context}} title="Martin's Stuff" date="2018-02-03"/>)
+		app.render(<HomePage {...{app, context}} title="Martin's Stuff" date="2018-03-14"/>)
 	})
 }
